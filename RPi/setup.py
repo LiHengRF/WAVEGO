e03408b79a58cfb76abb7b5c90219bfead49a04b
LiHengRF <149810925+LiHengRF@users.noreply.github.com>
2025-12-06 23:16:17 +0100

    Refactor setup.py for Debian 12 and PEP 668 compliance
    Updated setup script for Debian 12 compliance and improved package installation commands.

--- RPi/setup.py
+++ RPi/setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # File name   : setup.py for WAVEGO
 # Date        : 2022/1/5
+# Modified    : 2025 - Fixed for Debian 12+ (PEP 668 compliance)
 
 import os
 import time
@@ -9,139 +10,160 @@ import re
 curpath = os.path.realpath(__file__)
 thisPath = os.path.dirname(curpath)
 
-def replace_num(file,initial,new_num):  
-    newline=""
-    str_num=str(new_num)
-    with open(file,"r") as f:
+# Check if running as root
+if os.geteuid() != 0:
+    print("Error: This script must be run with sudo!")
+    print("Usage: sudo python3 setup.py")
+    exit(1)
+
+def replace_num(file, initial, new_num):  
+    newline = ""
+    str_num = str(new_num)
+    with open(file, "r") as f:
         for line in f.readlines():
             if(line.find(initial) == 0):
-                line = (str_num+'\n')
+                line = (str_num + '\n')
             newline += line
-    with open(file,"w") as f:
+    with open(file, "w") as f:
         f.writelines(newline)
 
-for x in range(1,4):
-	if os.system("sudo apt update") == 0:
-		break
+# System update
+for x in range(1, 4):
+    if os.system("apt update") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo apt -y dist-upgrade") == 0:
-		break
+for x in range(1, 4):
+    if os.system("apt -y dist-upgrade") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo apt clean") == 0:
-		break
+for x in range(1, 4):
+    if os.system("apt clean") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo pip3 install -U pip") == 0:
-		break
+# Upgrade pip (with --break-system-packages flag for PEP 668)
+for x in range(1, 4):
+    if os.system("pip3 install --break-system-packages -U pip") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo apt-get install -y python-dev python3-pip libfreetype6-dev libjpeg-dev build-essential") == 0:
-		break
+# Install system dependencies (changed python-dev to python3-dev)
+for x in range(1, 4):
+    if os.system("apt-get install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo apt-get install -y i2c-tools") == 0:
-		break
+for x in range(1, 4):
+    if os.system("apt-get install -y i2c-tools") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo apt-get install -y python3-smbus") == 0:
-		break
+for x in range(1, 4):
+    if os.system("apt-get install -y python3-smbus") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo pip3 install pyserial") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple pyserial") == 0:
-		break
+# Install Python packages (with --break-system-packages flag)
+for x in range(1, 4):
+    if os.system("pip3 install --break-system-packages pyserial") == 0:
+        break
+    elif os.system("pip3 install --break-system-packages -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple pyserial") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo pip3 install flask") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple flask") == 0:
-		break
+for x in range(1, 4):
+    if os.system("pip3 install --break-system-packages flask") == 0:
+        break
+    elif os.system("pip3 install --break-system-packages -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple flask") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo pip3 install flask_cors") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple flask_cors") == 0:
-		break
+for x in range(1, 4):
+    if os.system("pip3 install --break-system-packages flask_cors") == 0:
+        break
+    elif os.system("pip3 install --break-system-packages -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple flask_cors") == 0:
+        break
 
-for x in range(1,4):
-	if os.system("sudo pip3 install websockets") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple websockets") == 0:
-		break
+for x in range(1, 4):
+    if os.system("pip3 install --break-system-packages websockets") == 0:
+        break
+    elif os.system("pip3 install --break-system-packages -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple websockets") == 0:
+        break
+
+# Modify boot config
 try:
-	replace_num("/boot/config.txt",'[all]','[all]\nenable_uart=1\ngpu_mem=128')
+    replace_num("/boot/config.txt", '[all]', '[all]\nenable_uart=1\ngpu_mem=128')
 except:
-	print('try again')
-
-try:
-	replace_num("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1\nstart_x=1')
-except:
-	print('try again')
-
-try:
-	replace_num("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1')
-except:
-	print('try again')
-
-
-CMDLINE_FILE = open('/boot/cmdline.txt', 'r')
-OLD_LINES = CMDLINE_FILE.readlines()
-CMDLINE_FILE.close()
-
-CMDLINE_FILE = open('/boot/cmdline.txt', 'w+')
-for EACH_LINE in OLD_LINES:
-	NEW_LINES = re.sub('console=serial0,115200', '', EACH_LINE)
-	CMDLINE_FILE.writelines(NEW_LINES)
-
-CMDLINE_FILE.close()
-
-
-for x in range(1,4):
-	if os.system("sudo pip3 install opencv-contrib-python==3.4.11.45") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple opencv-contrib-python==3.4.11.45") == 0:
-		break
-
-
-
-for x in range(1,4):
-	if os.system("sudo pip3 uninstall -y numpy") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install numpy==1.21") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple numpy==1.21") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install imutils zmq pybase64 psutil") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple imutils zmq pybase64 psutil") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
-		break
-        
-for x in range(1,4):
-	if os.system("cd " + thisPath + " && cd .. && sudo git clone https://github.com/oblique/create_ap") == 0:
-		break
+    print('Failed to modify /boot/config.txt [all] section, try again')
 
 try:
-	os.system("cd " + thisPath + " && cd .. && cd create_ap && sudo make install")
+    replace_num("/boot/config.txt", 'camera_auto_detect=1', '#camera_auto_detect=1\nstart_x=1')
 except:
-	pass
+    print('Failed to modify camera_auto_detect, try again')
 
-replace_num('/etc/rc.local','exit 0','cd '+thisPath+' && sudo python3 webServer.py &\nexit 0')
+try:
+    replace_num("/boot/config.txt", 'camera_auto_detect=1', '#camera_auto_detect=1')
+except:
+    print('Failed to comment camera_auto_detect, try again')
 
-print('Completed!')
+# Modify cmdline.txt
+try:
+    CMDLINE_FILE = open('/boot/cmdline.txt', 'r')
+    OLD_LINES = CMDLINE_FILE.readlines()
+    CMDLINE_FILE.close()
 
-os.system("sudo reboot")
+    CMDLINE_FILE = open('/boot/cmdline.txt', 'w+')
+    for EACH_LINE in OLD_LINES:
+        NEW_LINES = re.sub('console=serial0,115200', '', EACH_LINE)
+        CMDLINE_FILE.writelines(NEW_LINES)
+    CMDLINE_FILE.close()
+except Exception as e:
+    print(f'Failed to modify /boot/cmdline.txt: {e}')
+
+# Install OpenCV (using newer compatible version)
+for x in range(1, 4):
+    if os.system("pip3 install --break-system-packages opencv-contrib-python") == 0:
+        break
+    elif os.system("pip3 install --break-system-packages -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple opencv-contrib-python") == 0:
+        break
+
+# Install numpy (compatible version)
+for x in range(1, 4):
+    if os.system("pip3 uninstall -y numpy") == 0:
+        break
+
+for x in range(1, 4):
+    if os.system("pip3 install --break-system-packages numpy") == 0:
+        break
+    elif os.system("pip3 install --break-system-packages -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple numpy") == 0:
+        break
+
+# Install HDF5 and other libraries
+for x in range(1, 4):
+    if os.system("apt-get -y install libhdf5-dev libhdf5-serial-dev libatlas-base-dev") == 0:
+        break
+
+# Install additional Python packages
+for x in range(1, 4):
+    if os.system("pip3 install --break-system-packages imutils zmq pybase64 psutil") == 0:
+        break
+    elif os.system("pip3 install --break-system-packages -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple imutils zmq pybase64 psutil") == 0:
+        break
+
+# Install network tools
+for x in range(1, 4):
+    if os.system("apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
+        break
+
+# Clone and install create_ap
+for x in range(1, 4):
+    if os.system("cd " + thisPath + " && cd .. && git clone https://github.com/oblique/create_ap") == 0:
+        break
+
+try:
+    os.system("cd " + thisPath + " && cd .. && cd create_ap && make install")
+except:
+    pass
+
+# Add webServer to startup
+try:
+    replace_num('/etc/rc.local', 'exit 0', 'cd ' + thisPath + ' && python3 webServer.py &\nexit 0')
+except Exception as e:
+    print(f'Failed to modify /etc/rc.local: {e}')
+
+print('Setup completed!')
+
+os.system("reboot")
